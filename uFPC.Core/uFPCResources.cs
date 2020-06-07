2714e42c2075db5220da23c9ae8bcbac8cf04f29
FungyBytes <contact@fungybytes.com>
2020-06-07 18:04:12 +0200

    initial commit backoffice dashboard

---
+++ uFPC.Core/uFPCResources.cs
@@ -0,0 +1,22 @@
+﻿using System.Text.RegularExpressions;
+using System.Web.Mvc;
+using uFPC.Attributes;
+using uFPC.Cache;
+using Umbraco.Core.Composing;
+using Umbraco.Web;
+using Umbraco.Web.Models;
+using Umbraco.Web.Mvc;
+
+namespace uFPC.Controllers
+{
+    [PluginController("uFPC")]
+    public class ResourcesController : UmbracoController
+    {
+        // E.G. /umbraco/ufpc/resources/update
+        [HttpGet]
+        public void Update()
+        {
+            uFPCCache.Update();
+        }
+    }
+}
